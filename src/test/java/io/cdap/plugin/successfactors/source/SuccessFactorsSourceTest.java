9a4f0d3cea7902597b357d2ca933f6d9e78d7aa7
Shubhangi-cs <shubhangi.garg@cloudsufi.com>
2022-07-27 13:51:39 +0530

    Junits-Part2

---
+++ src/test/java/io/cdap/plugin/successfactors/source/SuccessFactorsSourceTest.java
@@ -0,0 +1,467 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.cdap.plugin.successfactors.source;
+
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.etl.api.batch.BatchSourceContext;
+import io.cdap.cdap.etl.api.validation.ValidationException;
+import io.cdap.cdap.etl.api.validation.ValidationFailure;
+import io.cdap.cdap.etl.mock.common.MockPipelineConfigurer;
+import io.cdap.cdap.etl.mock.validation.MockFailureCollector;
+import io.cdap.plugin.successfactors.common.exception.SuccessFactorsServiceException;
+import io.cdap.plugin.successfactors.common.exception.TransportException;
+import io.cdap.plugin.successfactors.common.util.ResourceConstants;
+import io.cdap.plugin.successfactors.source.config.SuccessFactorsPluginConfig;
+import io.cdap.plugin.successfactors.source.input.SuccessFactorsInputSplit;
+import io.cdap.plugin.successfactors.source.input.SuccessFactorsPartitionBuilder;
+import io.cdap.plugin.successfactors.source.metadata.SuccessFactorsEntityProvider;
+import io.cdap.plugin.successfactors.source.metadata.SuccessFactorsSchemaGenerator;
+import io.cdap.plugin.successfactors.source.service.SuccessFactorsService;
+import io.cdap.plugin.successfactors.source.transport.SuccessFactorsResponseContainer;
+import io.cdap.plugin.successfactors.source.transport.SuccessFactorsTransporter;
+import io.cdap.plugin.successfactors.source.transport.SuccessFactorsUrlContainer;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Tested;
+import org.apache.olingo.odata2.api.edm.Edm;
+import org.apache.olingo.odata2.api.ep.EntityProvider;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SuccessFactorsSourceTest {
+
+  @Tested
+  private SuccessFactorsPluginConfig.Builder pluginConfigBuilder;
+  private SuccessFactorsSource successFactorsSource;
+  private SuccessFactorsPluginConfig pluginConfig;
+  private SuccessFactorsTransporter successFactorsTransporter;
+  private SuccessFactorsService successFactorsService;
+  private SuccessFactorsPartitionBuilder successFactorsPartitionBuilder;
+  private Schema schema;
+  private MockPipelineConfigurer pipelineConfigurer;
+  @Mocked
+  private Edm edm;
+  @Mocked
+  private BatchSourceContext context;
+  @Mocked
+  private EntityProvider entityProvider;
+  private SuccessFactorsUrlContainer successFactorsUrlContainer;
+  private SuccessFactorsSchemaGenerator successFactorsSchemaGenerator;
+
+
+  @Before
+  public void setUp() {
+    pipelineConfigurer = new MockPipelineConfigurer(null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("unit-test-ref-name")
+      .baseURL("http://localhost")
+      .entityName("entity name")
+      .username("username")
+      .password("password")
+      .selectOption("col1,col2,   \n  parent/col1,\r       col3     ");
+  }
+
+  @Test
+  public void testConfigurePipelineWithInvalidUrl() throws Exception {
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("unit-test-ref-name")
+      .baseURL("base_url")
+      .entityName("entity name")
+      .username("username")
+      .password("password")
+      .selectOption("col1,col2,   \n  parent/col1,\r       col3     ");
+    SuccessFactorsPluginConfig pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+    Map<String, Object> plugins = new HashMap<>();
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+    try {
+      successFactorsSource.configurePipeline(mockPipelineConfigurer);
+      Assert.fail("Exception is not thrown if valid url is provided");
+    } catch (ValidationException e) {
+      Assert.assertEquals("Please verify the provided base url is correct. Please contact the SAP " +
+                            "administrator.", e.getFailures().get(0).getMessage());
+    }
+  }
+
+  @Test
+  public void testConfigurePipelineWithInvalidReferenceName() {
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("http://localhost")
+      .entityName("entity name")
+      .username("username")
+      .password("password")
+      .selectOption("col1,col2,   \n  parent/col1,\r       col3     ");
+    try {
+      pluginConfig = pluginConfigBuilder.build();
+      successFactorsSource = new SuccessFactorsSource(pluginConfig);
+      Map<String, Object> plugins = new HashMap<>();
+      MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+      successFactorsSource.configurePipeline(mockPipelineConfigurer);
+      Assert.fail("Exception is not thrown if valid reference name is provided");
+    } catch (ValidationException ve) {
+      List<ValidationFailure> failures = ve.getFailures();
+      Assert.assertEquals(1, failures.size());
+      Assert.assertEquals("Invalid reference name ''.", ve.getFailures().get(0).getMessage());
+    }
+  }
+
+  private URL getUrl() throws MalformedURLException {
+    return new URL("http://localhost:8000/odata/v2/entity-name");
+  }
+
+  private SuccessFactorsResponseContainer getResponseContainer() {
+    return new SuccessFactorsResponseContainer(200, "ok",
+                                               "2.0", new byte[]{50});
+  }
+
+  private Schema getPluginSchema() throws IOException {
+    String schemaString = "{\"type\":\"record\",\"name\":\"SuccessFactorsColumnMetadata\",\"fields\":[{\"name\":" +
+      "\"backgroundElementId\",\"type\":\"long\"},{\"name\":\"bgOrderPos\",\"type\":\"long\"},{\"name\":" +
+      "\"description\",\"type\":[\"string\",\"null\"]},{\"name\":\"endDate\",\"type\":[{\"type\":\"long\"," +
+      "\"logicalType\":\"timestamp-micros\"},\"null\"]},{\"name\":\"lastModifiedDate\",\"type\":" +
+      "[{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"},\"null\"]},{\"name\":\"project\",\"type\":" +
+      "\"string\"},{\"name\":\"startDate\",\"type\":[{\"type\":\"long\",\"logicalType\":\"timestamp-micros\"}," +
+      "\"null\"]},{\"name\":\"userId\",\"type\":\"string\"}]}";
+
+    return Schema.parseJson(schemaString);
+  }
+
+  @Test
+  public void testConfigurePipelineWSchemaNotNull() throws SuccessFactorsServiceException, TransportException,
+    IOException {
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("unit-test-ref-name")
+      .baseURL("http://localhost")
+      .entityName("entity-name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsTransporter = new SuccessFactorsTransporter(pluginConfig.getUsername(), pluginConfig.getPassword());
+    successFactorsUrlContainer = new SuccessFactorsUrlContainer(pluginConfig);
+    successFactorsSchemaGenerator = new SuccessFactorsSchemaGenerator(new SuccessFactorsEntityProvider(edm));
+
+    new Expectations(SuccessFactorsUrlContainer.class, SuccessFactorsTransporter.class,
+                     SuccessFactorsSchemaGenerator.class) {
+      {
+        successFactorsUrlContainer.getTesterURL();
+        result = getUrl();
+        minTimes = 0;
+
+        successFactorsUrlContainer.getMetadataURL();
+        result = getUrl();
+        minTimes = 0;
+
+        successFactorsTransporter.callSuccessFactorsEntity(null, anyString, anyString);
+        result = getResponseContainer();
+        minTimes = 0;
+
+        successFactorsSchemaGenerator.buildDefaultOutputSchema(anyString);
+        result = getPluginSchema();
+        minTimes = 0;
+      }
+    };
+
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+    Map<String, Object> plugins = new HashMap<>();
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+    successFactorsSource.configurePipeline(pipelineConfigurer);
+    schema = pipelineConfigurer.getOutputSchema();
+    Assert.assertNotNull("Output Schema generated is not Null", schema);
+  }
+
+  @Test
+  public void testConfigurePipelineSchemaNull() {
+    SuccessFactorsPluginConfig pluginConfig = pluginConfigBuilder.build();
+    new Expectations(SuccessFactorsPluginConfig.class) {
+      {
+        pluginConfig.isSchemaBuildRequired();
+        result = false;
+        minTimes = 0;
+      }
+    };
+    Map<String, Object> plugins = new HashMap<>();
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+    successFactorsSource.configurePipeline(mockPipelineConfigurer);
+    Assert.assertNull(schema);
+  }
+
+  @Test(expected = ValidationException.class)
+  public void testConnectionException() {
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+    Map<String, Object> plugins = new HashMap<>();
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+    successFactorsSource.configurePipeline(mockPipelineConfigurer);
+  }
+
+  @Test
+  public void testConfigurePipelineConnectionException() {
+    pluginConfig = pluginConfigBuilder.build();
+    new Expectations(SuccessFactorsPluginConfig.class, SuccessFactorsTransporter.class) {
+      {
+        pluginConfig.isSchemaBuildRequired();
+        result = true;
+        minTimes = 0;
+      }
+    };
+    try {
+      successFactorsSource = new SuccessFactorsSource(pluginConfig);
+      Map<String, Object> plugins = new HashMap<>();
+      MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+      successFactorsSource.configurePipeline(mockPipelineConfigurer);
+      Assert.fail("Connection exception is not thrown if valid connection is provided");
+    } catch (ValidationException te) {
+      List<ValidationFailure> failures = te.getFailures();
+      Assert.assertEquals(1, failures.size());
+    }
+  }
+
+  private List<SuccessFactorsInputSplit> getSplits() {
+    List<SuccessFactorsInputSplit> splits = new ArrayList<>();
+
+    splits.add(new SuccessFactorsInputSplit(101, 120, 20));
+    splits.add(new SuccessFactorsInputSplit(121, 140, 20));
+    splits.add(new SuccessFactorsInputSplit(141, 160, 20));
+    splits.add(new SuccessFactorsInputSplit(161, 180, 20));
+    splits.add(new SuccessFactorsInputSplit(181, 200, 20));
+
+    return splits;
+  }
+
+  @Test
+  public void testPrepareRun() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    successFactorsPartitionBuilder = new SuccessFactorsPartitionBuilder();
+    successFactorsTransporter = new SuccessFactorsTransporter("username", "password");
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("unit-test-ref-name")
+      .baseURL("http://localhost")
+      .entityName("entity name")
+      .username("username")
+      .password("password")
+      .paginationType("serverSide")
+      .selectOption("col1,col2,   \n  parent/col1,\r       col3     ")
+      .filterOption("$topeq2");
+
+    pluginConfig = pluginConfigBuilder.build();
+
+    new Expectations(SuccessFactorsService.class, SuccessFactorsTransporter.class) {
+      {
+        context.getOutputSchema();
+        result = getPluginSchema();
+        minTimes = 0;
+
+        successFactorsTransporter.callSuccessFactors(null, anyString, anyString);
+        result = getResponseContainer();
+        minTimes = 0;
+
+        successFactorsPartitionBuilder.buildSplits(50);
+
+        result = getSplits();
+        minTimes = 0;
+
+        successFactorsService.getEncodedServiceMetadata();
+        result = "encodeMetadataString";
+        minTimes = 0;
+      }
+
+    };
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+    successFactorsSource.prepareRun(context);
+    Map<String, Object> plugins = new HashMap<>();
+    MockPipelineConfigurer mockPipelineConfigurer = new MockPipelineConfigurer(null, plugins);
+    schema = mockPipelineConfigurer.getOutputSchema();
+    MockFailureCollector failureCollector = new MockFailureCollector();
+    Assert.assertEquals(0, failureCollector.getValidationFailures().size());
+  }
+
+  @Test
+  public void testPrepareRunUnauthorizedError() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("")
+      .entityName("entity name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+
+    new Expectations(SuccessFactorsService.class) {
+      {
+        context.getOutputSchema();
+        result = null;
+        minTimes = 0;
+
+        successFactorsService.checkSuccessFactorsURL();
+        result = new SuccessFactorsServiceException("Unauthorized Access", 401);
+        minTimes = 0;
+      }
+    };
+
+    try {
+      successFactorsSource.prepareRun(context);
+      Assert.fail("Exception is not thrown with authorized one");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(ResourceConstants.ERR_MACRO_INPUT.getMsgForKeyWithCode(), e.getMessage());
+    }
+  }
+
+  @Test
+  public void testPrepareRunForbiddenError() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("")
+      .entityName("entity name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+
+    new Expectations(SuccessFactorsService.class) {
+      {
+        context.getOutputSchema();
+        result = null;
+        minTimes = 0;
+
+        successFactorsService.checkSuccessFactorsURL();
+        result = new SuccessFactorsServiceException("Forbidden Error", 403);
+        minTimes = 0;
+      }
+    };
+
+    try {
+      successFactorsSource.prepareRun(context);
+      Assert.fail("Exception is not thrown with authorized one");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(ResourceConstants.ERR_MACRO_INPUT.getMsgForKeyWithCode(), e.getMessage());
+    }
+  }
+
+  @Test
+  public void testPrepareRunNotFoundError() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("")
+      .entityName("entity name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+
+    new Expectations(SuccessFactorsService.class) {
+      {
+        context.getOutputSchema();
+        result = null;
+        minTimes = 0;
+
+        successFactorsService.checkSuccessFactorsURL();
+        result = new SuccessFactorsServiceException("Not Found Error", 404);
+        minTimes = 0;
+      }
+    };
+
+    try {
+      successFactorsSource.prepareRun(context);
+      Assert.fail("Exception is not thrown with authorized one");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(ResourceConstants.ERR_MACRO_INPUT.getMsgForKeyWithCode(), e.getMessage());
+    }
+  }
+
+  @Test
+  public void testPrepareRunBadRequestError() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("")
+      .entityName("entity name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+
+    new Expectations(SuccessFactorsService.class) {
+      {
+        context.getOutputSchema();
+        result = null;
+        minTimes = 0;
+
+        successFactorsService.checkSuccessFactorsURL();
+        result = new SuccessFactorsServiceException("Bad Request Error", 400);
+        minTimes = 0;
+      }
+    };
+
+    try {
+      successFactorsSource.prepareRun(context);
+      Assert.fail("Exception is not thrown with authorized one");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(ResourceConstants.ERR_MACRO_INPUT.getMsgForKeyWithCode(), e.getMessage());
+    }
+  }
+
+  @Test
+  public void testPrepareRunInvalidVersionError() throws Exception {
+    successFactorsService = new SuccessFactorsService(pluginConfig, null);
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .referenceName("")
+      .baseURL("")
+      .entityName("entity name")
+      .username("username")
+      .password("password");
+
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsSource = new SuccessFactorsSource(pluginConfig);
+
+    new Expectations(SuccessFactorsService.class) {
+      {
+        context.getOutputSchema();
+        result = null;
+        minTimes = 0;
+
+        successFactorsService.checkSuccessFactorsURL();
+        result = new SuccessFactorsServiceException("Invalid Version Error", 1);
+        minTimes = 0;
+      }
+    };
+
+    try {
+      successFactorsSource.prepareRun(context);
+      Assert.fail("Exception is not thrown with correct version");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(ResourceConstants.ERR_MACRO_INPUT.getMsgForKeyWithCode(), e.getMessage());
+    }
+  }
+}
---
+++ src/test/java/io/cdap/plugin/successfactors/source/transport/SuccessFactorsTransporterTest.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.successfactors.source.transport;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.cdap.plugin.successfactors.common.exception.TransportException;
+import io.cdap.plugin.successfactors.common.util.ResourceConstants;
+import io.cdap.plugin.successfactors.source.config.SuccessFactorsPluginConfig;
+import io.cdap.plugin.successfactors.source.metadata.TestSuccessFactorsUtil;
+import io.cdap.plugin.successfactors.source.service.SuccessFactorsService;
+import mockit.Mock;
+import mockit.MockUp;
+import okhttp3.OkHttpClient;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.ws.rs.core.MediaType;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+public class SuccessFactorsTransporterTest {
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
+  @Rule
+  public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort().dynamicHttpsPort());
+  private SuccessFactorsPluginConfig.Builder pluginConfigBuilder;
+  private SuccessFactorsTransporter transporter;
+  private SuccessFactorsPluginConfig pluginConfig;
+  private SuccessFactorsUrlContainer successFactorsURL;
+
+  @BeforeClass
+  public static void classSetup() {
+    new MockUp<SuccessFactorsTransporter>() {
+      @Mock
+      public OkHttpClient.Builder getConfiguredClient() {
+        return allowAllSSL();
+      }
+    };
+  }
+
+  private static OkHttpClient.Builder allowAllSSL() {
+    TrustManager[] trustAllCerts = new TrustManager[]{
+      new X509TrustManager() {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+          return new X509Certificate[]{};
+        }
+      }
+    };
+
+    try {
+      SSLContext trustAllSslContext = SSLContext.getInstance("SSL");
+      trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
+      HostnameVerifier customHostVerifier = new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      };
+
+      OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+      builder.sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+      builder.hostnameVerifier(customHostVerifier);
+      return builder;
+    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Before
+  public void setUp() {
+    pluginConfigBuilder = SuccessFactorsPluginConfig.builder()
+      .baseURL("https://localhost:" + wireMockRule.httpsPort())
+      .entityName("Entity")
+      .username("test")
+      .password("secret");
+    pluginConfig = pluginConfigBuilder.build();
+    successFactorsURL = new SuccessFactorsUrlContainer(pluginConfig);
+    transporter = new SuccessFactorsTransporter(pluginConfig.getUsername(), pluginConfig.getPassword());
+  }
+
+  @Test
+  public void testCallSuccessFactors() throws TransportException {
+    String expectedBody = "{\"d\": [{\"ID\": 0,\"Name\": \"Bread\"}}]}";
+    WireMock.stubFor(WireMock.get("/Entity?%24top=1")
+                       .withBasicAuth(pluginConfig.getUsername(), pluginConfig.getPassword())
+                       .willReturn(WireMock.ok()
+                                     .withHeader(SuccessFactorsTransporter.SERVICE_VERSION, "2.0")
+                                     .withBody(expectedBody)));
+    SuccessFactorsResponseContainer response = transporter
+      .callSuccessFactors(successFactorsURL.getTesterURL(), MediaType.APPLICATION_JSON, SuccessFactorsService.TEST);
+    Assert.assertEquals("SuccessFactors Service data version is same.",
+                        "2.0",
+                        response.getDataServiceVersion());
+    Assert.assertEquals("HTTP status code is same.",
+                        HttpURLConnection.HTTP_OK,
+                        response.getHttpStatusCode());
+    Assert.assertEquals("HTTP response body is same.",
+                        expectedBody,
+                        TestSuccessFactorsUtil.convertInputStreamToString(response.getResponseStream()));
+    Assert.assertEquals("HTTP status is same", "OK", response.getHttpStatusMsg());
+  }
+
+  @Test
+  public void testUnAuthorized() throws TransportException {
+    WireMock.stubFor(WireMock.get("/Entity/$metadata")
+                       .willReturn(WireMock.unauthorized()));
+    SuccessFactorsResponseContainer response = transporter
+      .callSuccessFactors(successFactorsURL.getMetadataURL(), MediaType.APPLICATION_XML,
+                          SuccessFactorsService.METADATA);
+    WireMock.verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/Entity/$metadata")));
+    Assert.assertEquals("HTTP status code is matching.",
+                        HttpURLConnection.HTTP_UNAUTHORIZED,
+                        response.getHttpStatusCode());
+  }
+
+  @Test
+  public void testInvalidHost() throws TransportException {
+    successFactorsURL = new SuccessFactorsUrlContainer(pluginConfigBuilder.baseURL("http://INVALID-HOST").build());
+    exception.expectCause(CoreMatchers.isA(UnknownHostException.class));
+    transporter.callSuccessFactors(successFactorsURL.getMetadataURL(), MediaType.APPLICATION_XML,
+                                   SuccessFactorsService.METADATA);
+  }
+
+  @Test
+  public void testConnectionTimeout() throws TransportException {
+    WireMock.stubFor(WireMock.get("/Entity?%24top=1")
+                       .willReturn(WireMock.aResponse()
+                                     .withFixedDelay(61_000)));
+    exception.expectMessage(ResourceConstants.ERR_CALL_SERVICE_FAILURE.getMsgForKey(SuccessFactorsService.TEST));
+    exception.expectCause(CoreMatchers.isA(SocketTimeoutException.class));
+    transporter.callSuccessFactors(successFactorsURL.getTesterURL(), MediaType.APPLICATION_JSON,
+                                   SuccessFactorsService.TEST);
+  }
+}
